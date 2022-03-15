4d581c5ac07f49fca87843d7c5e71151d674025d
olichose123 <oli.banville@gmail.com>
2022-03-15 13:00:00 -0400

    fix publish

--- micadoi/__main__.py
+++ micadoi/__main__.py
@@ -12,6 +12,7 @@ group.add_command(mica.extract)
 group.add_command(datacite.get_doi)
 group.add_command(datacite.generate_mica_doi)
 group.add_command(datacite.update_doi)
+group.add_command(datacite.publish_doi)
 
 if __name__ == "__main__":
     group()
